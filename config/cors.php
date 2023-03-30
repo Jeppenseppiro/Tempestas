df29bb324ab2faa59346e7bd3e2e88ec059fc9bb
Jeppenseppiro <jeffjurolan990@gmail.com>
2023-03-30 17:03:31 +0800

    fix: mixed content error

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tempestas-git-vercel-build-jeppenseppiro.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 

b11c1f71d8ac9e32f197c96c6753e0f6b661853c
Jeppenseppiro <jeffjurolan990@gmail.com>
2023-03-30 10:22:11 +0800

    TrustedProxies middlware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,8 +13,7 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies = [
-        'https://tempestas.vercel.app/',
-        '76.76.21.142'
+        '*'
     ];
 
     /**

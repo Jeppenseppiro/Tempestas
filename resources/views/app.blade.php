7bf9bfb33dcbea136bb04b8d6a9874008915417a
Jeppenseppiro <jeffjurolan990@gmail.com>
2023-03-31 16:47:32 +0800

    webapp meta preview

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,14 +11,14 @@
     <meta property="og:url" content="https://metatags.io/">
     <meta property="og:title" content="Tempestas">
     <meta property="og:description" content="A simple interactive weather web application based on the Visual Crossing weather API">
-    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="og:image" content={{ asset('images/meta_image.png') }}>
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://metatags.io/">
     <meta property="twitter:title" content="Tempestas">
     <meta property="twitter:description" content="A simple interactive weather web application based on the Visual Crossing weather API">
-    <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="twitter:image" content={{ asset('images/meta_image.png') }}>
     
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />

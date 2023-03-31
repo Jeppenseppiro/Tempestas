4812cca5047475c020cde0b1260bf9aa69c7f409
Jeppenseppiro <jeffjurolan990@gmail.com>
2023-03-31 14:38:17 +0800

    cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS'],
 
-    'allowed_origins' => ['https://tempestas-git-vercel-build-jeppenseppiro.vercel.app/, https://tempestas.vercel.app/'],
+    'allowed_origins' => ['https://tempestas-git-vercel-build-jeppenseppiro.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
